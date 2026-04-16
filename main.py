7b04bb457560c73060566e9d53f4c76b2f9816be
deniseantoniocanto-cmd <deniseantoniocanto@gmail.com>
2026-04-16 23:43:13 +0100

    correção do arquivo main

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ def avaliar_mensagem_comando(mensagem: str):
 
     pass
 
-
-def teste():
-    pass
-
-def avaliar_mensagem_comando() -> None:
-    pass
-
 def main():
     project_name: str = "projetogit"
     print(f"Hello from {project_name}!")
