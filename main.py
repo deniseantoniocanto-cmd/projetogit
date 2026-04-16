8a62e9be68893e930721c0c0a983986f514b55f2
deniseantoniocanto-cmd <deniseantoniocanto@gmail.com>
2026-04-17 00:37:25 +0100

    atualizado para dados do pc

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-def avaliar_mensagem_comando(mensagem: str):
+class Dados_pc:
+    def __init__ (self, marca, memoria, placa_video):
+        self.marca = marca
+        self.memoria = memoria
+        self.placa_video = placa_video
+       
 
-    pass
+    def main(self):
+        project_name: str = "projetogit"
+        print(f"\n====== Bem vindo ao {project_name} =======!")
+        
 
-def main():
-    project_name: str = "projetogit"
-    print(f"Hello from {project_name}!")
+    def ver_dados(self):
+        projeto_name: str = 'projetogit'
+        print('\n> A seguir dados do pc !!! ')
 
-    texto_usuario = ' Ola mundo! '
-    avaliar_mensagem_comando(texto_usuario)
+    def ligar_computador(self):
+        print('\nlingando...')
 
+    def informaçoes_pc(self):
+        print(f'{self.marca}, {self.memoria}, {self.placa_video}')
+
+    def desligar_pc(self):
+        print('desligando.\n')
+
+computador1 = Dados_pc('Asus', '16 gigas','NvDIA')
 
 if __name__ == "__main__":
-    main()
+    computador1 = Dados_pc('Asus', '16 gigas','NvDIA')
+    computador1.main()
+    computador1.ver_dados()
+    computador1.ligar_computador()
+    computador1.informaçoes_pc()
+    computador1.desligar_pc()
+
